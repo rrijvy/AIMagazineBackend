3eacff1c75e07e825d2e870671102f1370ccf959
rrijvy <32623510+rrijvy@users.noreply.github.com>
2024-09-27 02:25:11 +0600

    wip

--- src/common/utils/httpHandlers.ts
+++ src/common/utils/httpHandlers.ts
@@ -1,10 +1,9 @@
+import { ServiceResponse } from "@/common/models/serviceResponse";
 import type { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import type { ZodError, ZodSchema } from "zod";
 
-import { ServiceResponse } from "@/common/models/serviceResponse";
-
-export const handleServiceResponse = (serviceResponse: ServiceResponse<any>, response: Response) => {
+export const handleServiceResponse = (serviceResponse: ServiceResponse<unknown>, response: Response) => {
   return response.status(serviceResponse.statusCode).send(serviceResponse);
 };
 
@@ -16,6 +15,6 @@ export const validateRequest = (schema: ZodSchema) => (req: Request, res: Respon
     const errorMessage = `Invalid input: ${(err as ZodError).errors.map((e) => e.message).join(", ")}`;
     const statusCode = StatusCodes.BAD_REQUEST;
     const serviceResponse = ServiceResponse.failure(errorMessage, null, statusCode);
-    return handleServiceResponse(serviceResponse, res);
+    handleServiceResponse(serviceResponse, res);
   }
 };
