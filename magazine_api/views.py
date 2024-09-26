2ff65b486276d8fff67e1ac43b19dc9497118b23
rrijvy <rrijvy@gmail.com>
2024-09-26 20:05:14 +0600

    node project init

--- magazine_api/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-
-@api_view(["GET"])
-def getData(request):
-    person = {"name": "rijvy"}
-    return Response(person)
--- magazine_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from magazine_api import views
-
-urlpatterns = [
-    path("api/", views.getData),
-]
